162c281846f0f143d440c8f822e992c9685aea06
Armend Ukehaxhaj <armendd.u@hotmail.com>
2019-07-16 12:33:15 +0200

    Added fullpage.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,79 @@
+import ReactFullpage from '@fullpage/react-fullpage';
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from './Menu';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const SEL = 'custom-section';
+const SECTION_SEL = `.${SEL}`;
+
+const originalColors = ['#ff5f45', '#0798ec', '#fc6c7c', '#435b71', 'orange', 'blue', 'purple', 'yellow'];
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sectionsColor: [...originalColors],
+      fullpages: [
+        {
+          text: 'Section 1',
+        },
+        {
+          text: 'Section 2',
+        },
+        {
+          text: 'Section 3',
+        }
+      ],
+    };
+  }
+
+  onLeave(origin, destination, direction) {
+    console.log('onLeave', { origin, destination, direction });
+    // arguments are mapped in order of fullpage.js callback arguments do something
+    // with the event
+  }
+
+  handleChangeColors() {
+    const newColors =
+      this.state.sectionsColor[0] === 'yellow'
+        ? [...originalColors]
+        : ['yellow', 'blue', 'white'];
+    this.setState({
+      sectionsColor: newColors,
+    });
+  }
+
+  render() {
+    const { fullpages } = this.state;
+
+    if (!fullpages.length) {
+      return null;
+    }
+
+    return (
+      <div className="App">
+        <Menu />
+        <ReactFullpage
+          debug /* Debug logging */
+
+          // fullpage options
+          navigation
+          sectionSelector={SECTION_SEL}
+          onLeave={this.onLeave.bind(this)}
+          sectionsColor={this.state.sectionsColor}
+
+          render={comp => (
+            <ReactFullpage.Wrapper>
+              {fullpages.map(({ text }) => (
+                <div key={text} className={SEL}>
+                  <h1>{text}</h1>
+                </div>
+              ))}
+            </ReactFullpage.Wrapper>
+          )}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Menu.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Menu = () => (
+  <div
+    className="menu"
+    style={{
+      position: 'fixed',
+      top: 0,
+      zIndex: 100,
+    }}
+  >
+    <ul class="actions">
+      <li>
+        <button onClick={''}>About me</button>
+        <button onClick={''}>Projects</button>
+        <button onClick={() => this.handleChangeColors()}>
+          Change background colors
+          </button>
+        <button onClick={() => this.moveSectionDown()}>
+          Move Section Down
+          </button>
+      </li>
+    </ul>
+  </div>
+);
+
+export default Menu;
